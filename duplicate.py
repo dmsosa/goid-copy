53cd588cd6e5ee3bdd517b9da11f7de76edd7410
Durian <dmsosa21@outlook.com>
2023-07-15 15:50:04 +0100

    Neue Argumente hinzugefungen

--- duplicate.py
+++ duplicate.py
@@ -7,26 +7,39 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import datetime
+from urllib.parse import quote
+search = "Taj mahal"
 
-opt = Options()
-opt.add_experimental_option('detach', True)
-# opt.add_argument('--headless=new')
-serv = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=serv, options=opt)
 
-base = '&tbm=isch&sa=X&ved=2ahUKEwidnoqs_oGAAxUmVaQEHUMPDDcQ0pQJegQIDBAB&biw=1280&bih=648&dpr=1.5'
-root = 'https://www.google.com/search?q='
-url = root+'balls'+base
-driver.get(url)
-time.sleep(2)
-accept = driver.find_element(By.CSS_SELECTOR, 'button[jsname="b3VHJd"]')
-accept.click()
-time.sleep(2)
-imgs = driver.find_elements(By.CSS_SELECTOR, 'div[jsname="DeysSe"]')
-print(imgs)
+s = [str(datetime.datetime.now()).split('.')[0]]
+i = 0
+while i < len(s):
+    print(len(s))
+    print(i)
+    search = s[i]
+    print(search)
+    i += 1
 
-links = list()
-for i in imgs:
-    img = i.find_element(By.CSS_SELECTOR, 'img[jsname="Q4LuWd"]')
-    link = img.get_attribute('src')
-    links.append(link)
+# opt = Options()
+# opt.add_experimental_option('detach', True)
+# # opt.add_argument('--headless=new')
+# serv = Service(ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=serv, options=opt)
+
+# base = '&tbm=isch&sa=X&ved=2ahUKEwidnoqs_oGAAxUmVaQEHUMPDDcQ0pQJegQIDBAB&biw=1280&bih=648&dpr=1.5'
+# root = 'https://www.google.com/search?q='
+# url = root+'balls'+base
+# driver.get(url)
+# time.sleep(2)
+# accept = driver.find_element(By.CSS_SELECTOR, 'button[jsname="b3VHJd"]')
+# accept.click()
+# time.sleep(2)
+# imgs = driver.find_elements(By.CSS_SELECTOR, 'div[jsname="DeysSe"]')
+# print(imgs)
+
+# links = list()
+# for i in imgs:
+#     img = i.find_element(By.CSS_SELECTOR, 'img[jsname="Q4LuWd"]')
+#     link = img.get_attribute('src')
+#     links.append(link)
--- my.py
+++ my.py
@@ -1,25 +1,36 @@
+# In[ ]:
+# -*- coding: utf-8 -*-
+
 #///////////////// Programm zu starten /////////////////
 
 #die notwendig Modulen zu importieren
 
-import time #Wie mochten es wissen, wie lang unsere Programm zu beenden annehmen
-import sys, os #Sys und os Library zu importieren
-import ssl
+import time, datetime #Wie mochten es wissen, wie lang unsere Programm zu beenden annehmen
+import sys, os, ssl #Sys und os Library zu importieren
 from urllib import request
+from urllib.error import HTTPError, URLError
+from urllib.parse import quote
 import argparse
 
+sys.stdout.reconfigure(encoding='utf-8')
 parser = argparse.ArgumentParser()
-parser.add_argument('-k', '--keywords', help='Delimited keywords for searching images', required=True)
+parser.add_argument('-k', '--keywords', help='Delimited keywords for searching images', required=False)
 parser.add_argument('-l', '--limit', help='Delimited list input', required=False)
 parser.add_argument('-d', '--draw', help='Filtering by draw', required=False, const=True, action='store_const')
 parser.add_argument('-c', '--color', help='Filtering by color', required=False, choices=[
     'red','yellow','blue','orange','violet','green','brown','white','black','gray','pink','teal','purple'
 ])
-parser.add_argument('-u', '--url', help='Ein einzelnes Bilder nach Benutzerurl herunterladen', required=False, type=str)
+parser.add_argument('-u', '--url', help='Die Bildern nach Benutzerurl herunterladen werden', required=False, type=str)
+parser.add_argument('-o', '--output', help='Entmoglich dir, den Name von Ausgeben zu auswahlen', required=False, type=str)
+parser.add_argument('-s', '--single', help='Macht es moglich, ein einzelnes Bilder zu herunterladen', required=False, type=str)
+parser.add_argument('-p', '--pause', help='Bezeicht die Zeit, dass wir warten wird zwischen Bildern', required=False, type=str)
 
 args = parser.parse_args()
 
-search_keyword = [str(i) for i in args.keywords.split(',')]
+
+
+if (args.keywords is None) and (args.url is None) and (args.single is None):
+    parser.error('Keywordsargument obligatorisch ist!')
 
 if args.limit:
     limit = int(args.limit)
@@ -28,8 +39,17 @@ if args.limit:
 else:
     limit = 100
 
+if args.output:
+    main_dir = args.output
+else:
+    main_dir = 'downloads'
+
+if args.pause:
+    try:
+        pause = int(args.pause)
+    except ValueError:
+        parser.error('Die Pause muss ein Zahl sein!')
 #Globalen Variablen initialisieren
-keywords = ['', ' high quality', ' in real life', ' how to draw']
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
                 }
@@ -79,34 +99,51 @@ def _images_get_all_images(page):
         #Alle die Items in dem List "Links" bekannt zu hinzufugen
         #Timer konnte verwendet werden, um den Anfrage fur das Herunterladen von Bildern zu verlangsamen
             page = page[end_content:]
+            if len(items) > limit:
+                break
     return items
 
 #Die tatsache programm
 
-if args.url:
-    url = args.url;
+t0 = time.time()
+
+if args.single:
+    url = args.single
     try:
-        os.makedirs('downloads')
+        os.makedirs(main_dir)
     except OSError as e:
         if e.errno != 17:
             raise
         pass
-    req = request.Request(url, headers=headers)
-    response = request.urlopen(req, data=None, timeout=10, context=ctx)
-    imgName = url[(url.rfind('/')+1):]
+    try:
+        req = request.Request(url, headers=headers)
+        response = request.urlopen(req, None, timeout=10, context=ctx)
+        imgName = url[(url.rfind('/')+1):]
+    except HTTPError:
+        print("HTTP Fehler!")
+    except URLError:
+        print("URL Fehler!")
+    except IOError:
+        print("IO Fehler!")
+
     if '?' in imgName:
         imgName = imgName[:(imgName.find('?'))]
     if ('.jpg' in imgName) or ('.jpeg' in imgName) or ('.png' in imgName) or ('.svg' in imgName) or ('.tbn' in imgName):
         imgOutput = imgName
     else:
         imgOutput = imgName + ".jpg"
-    with open('downloads/'+imgOutput, 'wb') as saver:
+    with open(main_dir+imgOutput, 'wb') as saver:
         data = response.read()
         saver.write(data)
-    print("Bilder erfolgreich gespeichert! =====> "+imgOutput)
 
+    t1 = time.time()
+    total_time = t1 - t0
+
+    print("Bilder erfolgreich gespeichert! =====> "+imgOutput)
+    print("Programm Erfullt in "+str(total_time)+" Sekunden")
+    quit()
 else:
-    t0 = time.time()
+    
     i = 0
 
     root = 'https://www.google.com/search?q='
@@ -114,33 +151,75 @@ else:
     color_param = ('&tbs=ic:specific,isc:'+str(args.color) if args.color else '')
     draw_param = ('&tbs=ic:gray,itp:lineart' if args.draw else '')
 
+    if args.url:
+        search_keyword = str(datetime.datetime.now()).split('.')[0].split()[0]
+    else:
+        search_keyword = [str(i) for i in args.keywords.split(',')]
 
     while i < len(search_keyword):
         items = list()
-        iteration = "Item no.: " + str(i+1) + " -->" + " Item name = " + str(search_keyword[i])
-        print (iteration)
-        # with open('./links.txt', 'a', encoding='utf-8') as info:
-        #     info.write(str(i+1)+": "+str(search_keyword[i])+"\n")
-        search_keywords = search_keyword[i]
-        search = search_keywords.replace(' ', '%20')
-        dir_name = search_keywords + (" - " + str(args.color) if args.color else '')
+        if args.url:
+            url = args.url
+            dir_name = search_keyword + (" - " + str(args.color) if args.color else '')
+            sub_directory = os.path.join(main_dir, dir_name)
+            try:
+                if not os.path.exists(sub_directory):
+                    os.makedirs(sub_directory)
+            except OSError as e:
+                if e.errno != 17:
+                    raise
+            pass
+            try:
+                req = request.Request(url, headers=headers)
+                response = request.urlopen(req, timeout=10, context=ctx)
+                data = response.read()
+                imgName = url[url.rfind('/')+1:]
+                if '?' in imgName:
+                    imgName = imgName[:(imgName.find('?'))]
+                if ('.jpg' in imgName) or ('.jpeg' in imgName) or ('.png' in imgName) or ('.svg' in imgName) or ('.tbn' in imgName):
+                    imgOutput = imgName
+                else:
+                    imgOutput = imgName + ".jpg"
+                
+                saver = open(os.path.join(sub_directory,imgOutput), "wb")
+                saver.write(data)
+                saver.close()
+                t1 = time.time()
+                total_time = t1 - t0
+                print("Bilder erfolglich gespeichern! ===> "+imgName+"\nProgramm erfullt in "+str(total_time)+" Sekunden")
+                quit()
+            except HTTPError:
+                print('HTTP Fehler!')
+                quit()
+            except URLError:
+                print('HTTP Fehler!')
+                quit()
+            except IOError:
+                print('HTTP Fehler!')
+                quit()
+                
+            
 
-        try:
-            os.makedirs(dir_name)
-        except OSError as e:
-            if e.errno != 17:
-                raise
-        pass
-
-        j = 0 
-        while j < len(keywords):
-            coded_keyword = keywords[j].replace(' ', '%20')
-            url = root + search + coded_keyword + base + color_param + draw_param
-            page = download_page(url)
-            time.sleep(0.05)
-            print(coded_keyword)
-            items = items + _images_get_all_images(page)
-            j += 1
+        else: 
+            iteration = "Item no.: " + str(i+1) + " -->" + " Item name = " + str(search_keyword[i])
+            print (iteration)
+            with open('./links.txt', 'a', encoding='utf-8') as info:
+                info.write(str(i+1)+": "+str(search_keyword[i])+"\n")
+            search_keywords = search_keyword[i]
+            search = quote(search_keywords)
+            dir_name = search_keywords + (" - " + str(args.color) if args.color else '')
+            sub_directory = os.path.join(main_dir, dir_name)
+            try:
+                if not os.path.exists(sub_directory):
+                    os.makedirs(sub_directory)
+            except OSError as e:
+                if e.errno != 17:
+                    raise
+            pass
+            url = root + search + base + color_param + draw_param
+        page = download_page(url)
+        time.sleep(0.05)
+        items = items + _images_get_all_images(page)
 
 
         print ("Image Links = "+str(items))
@@ -151,9 +230,9 @@ else:
         #Links.txt zu erstellen
         
         #Dem Datei schreiben
-        # with open('./links.txt', 'a', encoding='utf-8') as info:
-        #     info.write(str(items)+"\n\n\n")
-        # #Dem Datei zu schliessen
+        with open('./links.txt', 'a', encoding='utf-8') as info:
+            info.write(str(items)+"\n\n\n")
+        #Dem Datei zu schliessen
         i += 1
 
         t1 = time.time() 
@@ -165,16 +244,16 @@ else:
     #Bildern speichern
         k = 0
         errors = 0
-        directory = 'downloads'
+    
 
 
         while k < len(items):
-            from urllib.error import HTTPError, URLError
             try:
                 req = request.Request(items[k], headers=headers)
                 response = request.urlopen(req, timeout=10, context=ctx)
                 data = response.read()
-                saver = open(os.path.join(dir_name,str(k+1)+".jpg"), "wb")
+
+                saver = open(os.path.join(sub_directory,str(k+1)+".jpg"), "wb")
                 saver.write(data)
                 saver.close()
                 print("Bild "+str(k+1)+" gespeichert")
@@ -196,3 +275,5 @@ else:
     #     /////////////////  Ende des Programm  /////////////////
 
 
+
+# %%
